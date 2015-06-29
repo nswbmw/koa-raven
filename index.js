1646d37b0b47489502bcd06539d04de85cad95f6
nswbmw <gxqzk@126.com>
2015-06-29 16:44:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var raven = require('raven');
+var Client = raven.Client;
+var parsers = raven.parsers;
+
+module.exports = function sentry(app, client) {
+  if (typeof client !== 'object') {
+    client = new Client(client || process.env.SENTRY_DSN, {level: process.env.SENTRY || 'error'});
+  }
+
+  // catch global errors
+  client.patchGlobal(function() {
+    process.exit(1);
+  });
+
+  var onerror = app.context.onerror;
+  var server_name = app.name || process.env.SENTRY_NAME;
+
+  app.context.onerror = function (err) {
+    if (!err) {
+      return;
+    }
+
+    var kwargs = parsers.parseRequest(this.req);
+    kwargs.server_name = server_name;
+    client.captureError(err, kwargs);
+    onerror.call(this, err);
+  };
+};
